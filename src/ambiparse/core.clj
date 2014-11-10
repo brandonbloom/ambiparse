389f5654813f9c5abafc12dbe7905b61984893c5
Brandon Bloom <brandon@brandonbloom.name>
2014-11-09 22:22:59 -0500

    Complete rework: Actually parses! Not usefully yet...

--- src/ambiparse/core.clj
+++ src/ambiparse/core.clj
@@ -1,96 +1,302 @@
 (ns ambiparse.core
-  (:refer-clojure :exclude [derive empty concat]))
+  (:refer-clojure :exclude [derive])
+  (:require [clojure.set :as set]
+            [clojure.math.combinatorics :refer [cartesian-product]]))
 
-(defprotocol Parser
-  (nullable? [this])
-  (derive [this c])
-  (form [this]))
 
-(extend-protocol Parser
-  clojure.lang.Var
-  (nullable? [this] (nullable? @this))
-  (derive [this c] (derive @this c))
-  (form [this] this))
+(defn lit [x]
+  {:head :lit :value x})
 
-(defn direct [x]
-  (if (var? x)
-    (recur @x)
-    x))
+(defn cat [& xs]
+  {:head :cat :elements (vec xs)})
 
-(def empty
-  (reify Parser
-    (nullable? [_] false)
-    (derive [_ c] empty)
-    (form [_] 'empty)))
+(defn alt [& xs]
+  {:head :alt :alternatives (set xs)})
 
-(def epsilon
-  (reify Parser
-    (nullable? [_] true)
-    (derive [_ c] empty)
-    (form [_] 'epsilon)))
+(defn prod [x]
+  (assert (var? x))
+  {:head :prod :var x})
 
-(defn literal [c]
-  (reify Parser
-    (nullable? [_] false)
-    (derive [_ in-c] (if (= c in-c) epsilon empty))
-    (form [_] (list 'literal c))))
+(defn red [tag lang]
+  {:head :red :tag tag :lang lang})
 
-(defn union [left right]
-  (let [memo (atom {})
-        null (atom nil)]
-    (reify Parser
-      (nullable? [_]
-        (if-some [x @null]
-          x
-          (let [step #(reset! null (or (nullable? left) (nullable? right)))]
-            (reset! null false)
-            (reduce (fn [prev cur] (if (= prev cur) (reduced cur) cur))
-                    (repeatedly step)))))
-      (derive [_ c]
-        (or (@memo c)
-            (let [l (direct left), r (direct right)
-                  ret (cond
-                        (and (= l empty) (= r empty)) empty
-                        (= l empty) (derive r c)
-                        (= r empty) (derive l c)
-                        :else (union (derive l c) (derive r c)))]
-              (swap! memo assoc c ret)
-              ret)))
-      (form [this]
-        (list 'union (form left) (form right))))))
+(def null (cat))
+(def none (alt))
 
-(defn concat [left right]
-  (if (= (direct right) empty)
-    left
-    (reify Parser
-      (nullable? [_]
-        (and (nullable? left) (nullable? right)))
-      (derive [_ c]
-        (if (nullable? left)
-          (union (concat (derive left c) right)
-                 (derive right c))
-          (concat (derive left c) right)))
-      (form [this]
-        (list 'concat (form left) (form right))))))
 
-(defn matches? [parser input]
-  (if-let [[x & xs] (seq input)]
-    (and (not= (direct parser) epsilon)
-         (recur (derive parser x) xs))
-    (nullable? parser)))
+(defmacro defprod [name node]
+  `(do
+     (def ~name (prod (var ~name)))
+     (alter-meta! (var ~name) assoc ::lang (expand ~node))
+     (var ~name)))
 
-(comment
+(defn prod->red [prod]
+  (assert (= (:head prod) :prod))
+  (let [tag (let [m (-> prod :var meta)]
+              (keyword (-> m :ns str) (-> m :name str)))
+        lang (-> prod :var meta ::lang)]
+    (red tag lang)))
 
-  (do
-    (def A (union (concat (literal \a) #'A) epsilon))
-    (def B (union (concat (literal \b) #'B) epsilon))
-    (def AB (concat #'A #'B))
-    (-> #'AB
-        ;(derive \a)
-        ;(derive \a)
-        ;form
-        ;nullable?
-        (matches? "aaabbbbbbbbbbb")
-        ))
 
-)
+(defn parsed [trees]
+  (assert (set? trees))
+  {:head :parsed :trees trees})
+
+
+(defmulti expand1 class)
+
+(defmethod expand1 java.lang.Character [x]
+  (lit x))
+
+(defmethod expand1 clojure.lang.IPersistentMap [x]
+  x)
+
+(defmethod expand1 clojure.lang.IPersistentVector [x]
+  {:head :cat :elements x})
+
+(defmethod expand1 String [x]
+  {:head :cat :elements (vec x)})
+
+(defmethod expand1 clojure.lang.IPersistentSet [x]
+  {:head :alt :alternatives x})
+
+(defmethod expand1 clojure.lang.Var [x]
+  (prod x))
+
+
+(defmulti expand (fn [x] (:head x)))
+
+(defmethod expand nil [x] (expand (expand1 x)))
+
+(defmethod expand :lit [lang] lang)
+
+(defmethod expand :alt [lang]
+  (update-in lang [:alternatives] #(set (map expand %))))
+
+(defmethod expand :cat [lang]
+  (update-in lang [:elements] #(mapv expand %)))
+
+(defmethod expand :prod [lang]
+  lang)
+
+
+
+(def ^:dynamic *cache*)
+
+(defmacro with-cache [& body]
+  `(binding [*cache* {}]
+     ~@body))
+
+(defn memo [key thunk]
+  (if-some [ret (*cache* key)]
+    ret
+    (let [ret (thunk)]
+      (set! *cache* (assoc *cache* key ret))
+      ret)))
+
+(defn fix [key init step]
+  (if-some [ret (*cache* key)]
+    ret
+    (let [step #(let [x (step)]
+                  (set! *cache* (assoc *cache* key (step)))
+                  x)]
+      (set! *cache* (assoc *cache* key init))
+      (reduce (fn [prev cur] (if (= prev cur) (reduced cur) cur))
+              (repeatedly step)))))
+
+
+(defmulti nullable? :head)
+
+(defmethod nullable? :lit [node] false)
+
+(defmethod nullable? :parsed [node] true)
+
+(defmethod nullable? :cat [{:keys [elements]}]
+  (empty? elements))
+
+(defmethod nullable? :alt
+  [{:keys [alternatives]}]
+  (boolean (some nullable? alternatives)))
+
+(defmethod nullable? :red
+  [{:keys [lang]}]
+  (nullable? lang))
+
+(defmethod nullable? :prod
+  [{:keys [var] :as prod}]
+  (fix [:nullable? var]
+       false
+       (let [lang (prod->red prod)]
+         #(nullable? lang))))
+
+
+(defmulti null? :head)
+
+(defmethod null? :lit [node] false)
+
+(defmethod null? :parsed [node] false)
+
+(defmethod null? :cat [{:keys [elements]}]
+  (every? null? elements))
+
+(defmethod null? :alt
+  [{:keys [alternatives] :as lang}]
+  (and (not= lang none)
+       (every? null? alternatives)))
+
+(defmethod null? :red
+  [{:keys [lang]}]
+  (null? lang))
+
+(defmethod null? :prod
+  [{:keys [var] :as prod}]
+  (fix [:null? var]
+       true
+       (let [lang (prod->red prod)]
+         #(null? lang))))
+
+
+(defmulti none? :head)
+
+(defmethod none? :lit [node] false)
+
+(defmethod none? :parsed [node] false)
+
+(defmethod none? :cat
+  [{:keys [elements]}]
+  (boolean (some none? elements)))
+
+(defmethod none? :alt
+  [{:keys [alternatives]}]
+  (every? none? alternatives))
+
+(defmethod none? :red
+  [{:keys [lang]}]
+  (none? lang))
+
+(defmethod none? :prod
+  [{:keys [var] :as prod}]
+  (fix [:none? var]
+       true
+       (let [lang (prod->red prod)]
+         #(none? lang))))
+
+
+(defmulti parses :head)
+
+(defmethod parses :parsed [{:keys [trees]}] trees)
+
+(defmethod parses :lit [_] #{})
+
+(->> '(#{[\a]} #{[\x] [\y]})
+     (apply cartesian-product)
+     (map #(apply concat %))
+     )
+
+(defmethod parses :cat [{:keys [elements]}]
+  (->> (map parses elements)
+       (apply cartesian-product)
+       (map #(apply concat %))
+       set))
+
+(defmethod parses :alt [{:keys [alternatives]}]
+  (apply set/union (map parses alternatives)))
+
+(defmethod parses :red [{:keys [tag lang]}]
+  (set (for [p (parses lang)]
+         [(into [tag] p)])))
+
+(defmethod parses :prod [x]
+  (fix [:parses x]
+       #{}
+       #(parses (prod->red x))))
+
+
+(def ^:dynamic *simplifying* #{})
+
+(defmulti simplify :head)
+
+(defmethod simplify :lit [x] x)
+(defmethod simplify :parsed [x] x)
+
+(defmethod simplify :cat
+  [{:keys [elements]}]
+  (let [elms (->> elements
+                  (map simplify)
+                  (mapcat #(or (:elements %) [%]))
+                  (remove null?))]
+    (case (count (take 2 elms))
+      0 null
+      1 (first elms)
+      (apply cat elms))))
+
+(defmethod simplify :alt
+  [{:keys [alternatives]}]
+  (let [alts (->> alternatives
+                  (map simplify)
+                  (mapcat #(or (:alternatives %) #{%}))
+                  (remove none?))]
+    (case (count (take 2 alts))
+      0 none
+      1 (first alts)
+      (apply alt alts))))
+
+(defmethod simplify :red
+  [{:keys [tag lang]}]
+  (red tag (simplify lang)))
+
+(defmethod simplify :prod
+  [prod]
+  (let [lang (prod->red prod)]
+    (if (*simplifying* prod)
+      lang
+      (binding [*simplifying* (conj *simplifying* prod)]
+        (memo [:simplify prod]
+              #(simplify lang))))))
+
+
+
+;; Assumes language has been pre-simplified.
+(defmulti derive (fn [{:keys [head]} x] head))
+
+(defmethod derive :parsed [x c] none)
+
+(defmethod derive :lit
+  [{:keys [value]} x]
+  (if (= value x)
+    (parsed #{[x]})
+    none))
+
+(defmethod derive :cat
+  [{:keys [elements]} x]
+  ;;TODO can this be simpler?
+  (if (seq elements)
+    (let [[y & ys] elements]
+      (if (nullable? y)
+        (alt (cat (parsed (parses y)) (derive (apply cat ys) x))
+             (apply cat (derive y x) ys))
+        (apply cat (derive y x) ys)))
+    none))
+
+(defmethod derive :alt
+  [{:keys [alternatives]} x]
+  (apply alt (map #(derive % x) alternatives)))
+
+(defmethod derive :red
+  [{:keys [tag lang]} x]
+  (red tag (derive lang x)))
+
+(defmethod derive :prod
+  [prod x]
+  (memo [:derive prod]
+        #(derive (prod->red prod) x)))
+
+
+
+(defn parse [lang input]
+  (with-cache
+    (loop [parser (expand lang)
+           xs (seq input)]
+      (if-let [[x & xs] xs]
+        (recur (simplify (derive parser x)) xs)
+        (parses parser)))))
--- test/ambiparse/core_test.clj
+++ test/ambiparse/core_test.clj
@@ -1,7 +1,149 @@
 (ns ambiparse.core-test
-  (:require [clojure.test :refer :all]
-            [ambiparse.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is are]]
+            [ambiparse.core :as lang :refer [cat alt lit prod defprod]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(declare yx-list)
+
+(defprod xy-list #{[\x #'yx-list]
+                   #{\x []}})
+
+(defprod yx-list #{[\y xy-list]
+                   \y})
+
+(defprod a-list #{[#'a-list \a] \a})
+
+(defprod a-list-a #{[a-list \a] ""})
+
+
+
+(deftest expand-test
+  (are [x y] (= (lang/expand x) y)
+
+    []          (cat)
+    #{}         (alt)
+    \x          (lit \x)
+    "abc"       (cat (lit \a) (lit \b) (lit \c))
+    #{\a "xy"}  (alt (lit \a) (cat (lit \x) (lit \y)))
+    [(lit \x)]  (cat (lit \x))
+
+    #'yx-list   (prod #'yx-list)
+    yx-list     (prod #'yx-list)
+
+    ))
+
+(defn nullable? [lang]
+  (lang/with-cache
+    (-> lang lang/expand lang/nullable?)))
+
+(defn null? [lang]
+  (lang/with-cache
+    (-> lang lang/expand lang/null?)))
+
+(defn none? [lang]
+  (lang/with-cache
+    (-> lang lang/expand lang/none?)))
+
+(deftest nullable?-test
+  (are [lang bool] (= (nullable? lang) bool)
+
+    \x      false
+    ""      true
+    #{}     false
+    [\x \y] false
+
+    xy-list  true
+    yx-list  false
+    a-list   false
+    a-list-a true
+
+    ))
+
+(deftest null?-test
+  (are [lang bool] (= (null? lang) bool)
+
+    \x false
+    [] true
+    [[]] true
+    [[] \x] false
+    #{} false
+    #{\x []} false
+
+    ))
+
+(deftest none?-test
+  (are [lang bool] (= (none? lang) bool)
+
+    \x false
+    [] false
+    [[]] false
+    [[] \x] false
+    #{} true
+    #{#{}} true
+    #{#{} \x} false
+
+    ))
+
+(deftest simplify-test
+  (are [x y] (= (-> x lang/expand lang/simplify) (lang/expand y))
+
+    \x                     \x
+    []                     []
+    [\x]                   \x
+    ["abc"]                "abc"
+    [""]                   []
+    ["x"]                  \x
+    [[] \x []]             \x
+    [["x"]]                \x
+    ["abc" "xyz"]          "abcxyz"
+    [#{}]                  #{}
+    #{[]}                  []
+    #{}                    #{}
+    #{\x}                  \x
+    #{#{\x \y} #{\y \z}}   #{\x \y \z}
+    #{#{#{\x}}}            \x
+    #{\x ""}               #{\x []}
+
+    ))
+
+(deftest parse-test
+  (are [lang in out] (= (lang/parse lang in) out)
+
+    "a"       "b"        #{}
+    "a"       "aXc"      #{}
+
+    "a"       "a"        #{[\a]}
+    "abc"     "abc"      #{[\a \b \c]}
+    #{\a \b}  "a"        #{[\a]}
+
+    ;;TODO test with application of reduction rules
+    xy-list   "xyxy"     #{[[::xy-list \x
+                             [::yx-list \y
+                              [::xy-list \x
+                               [::yx-list \y]]]]]
+                           [[::xy-list \x
+                             [::yx-list \y
+                              [::xy-list \x
+                               [::yx-list \y
+                                [::xy-list]]]]]]}
+
+    ))
+
+(comment
+
+  (lang/parse "abc" "abc")
+
+  (lang/with-cache
+    (-> xy-list
+        (lang/parse "xyxy")
+        ;lang/expand
+        ;(lang/derive \x)
+        ;lang/simplify
+        ;lang/parses
+        fipp.edn/pprint
+        ))
+
+  (lang/parses (lang/alt (lang/lit \x)))
+
+)
+
