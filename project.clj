4ca2fdbca42db734452c8b437e964f9d5291376e
Brandon Bloom <brandon@brandonbloom.name>
2016-12-01 11:43:25 -0800

    fix project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gll "0.1.0-SNAPSHOT"
+(defproject ambiparse "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
